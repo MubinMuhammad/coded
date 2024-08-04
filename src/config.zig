7316d596bc5f228fd60f9f3211bacbd70111849f
Mubin Muhammad <mubin.muhammad.the.7th@gmail.com>
2024-08-04 19:40:39 +0600

    working on reading config...5% done

--- src/config.zig
+++ src/config.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 
+const ConfigError = error{
+    EmptyLine,
+    InvalidCommand,
+    NotEnoughArgs,
+};
+
 const Config = struct {
     setBuf: std.BufMap,
     changeBuf: std.BufMap,
+    themeBuf: std.BufMap,
     pathBuf: std.BufMap,
-    themesBuf: std.BufMap,
 
     pub fn init(configPath: []const u8, internalConfigPath: []const u8) !Config {
         var file = try std.fs.cwd().openFile(configPath, .{});
@@ -17,4 +23,20 @@ const Config = struct {
 
         while (try inputStream.readUntilDelimiterOrEof(&lineBuf, " ")) |line| {}
     }
+
+    pub fn deinit(self: *Config) void {
+        self.*.pathBuf.deinit();
+        self.*.changeBuf.deinit();
+        self.*.themeBuf.deinit();
+        self.*.pathBuf.deinit();
+    }
+
+    fn initConfig(cfg: *Config) void {
+        cfg.*.setBuf = std.BufMap.init(std.heap.page_allocator);
+        cfg.*.changeBuf = std.BufMap.init(std.heap.page_allocator);
+        cfg.*.themeBuf = std.BufMap.init(std.heap.page_allocator);
+        cfg.*.pathBuf = std.BufMap.init(std.heap.page_allocator);
+    }
+
+    fn getCommandArgs(line: []const u8, cfg: *Config) void {}
 };
