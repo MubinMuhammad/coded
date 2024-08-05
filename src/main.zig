3349cec30049087aa02681a6d910d1c5065596ce
Mubin Muhammad <mubin.muhammad.the.7th@gmail.com>
2024-08-05 18:31:30 +0600

    working on reading config...7% done

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
+const coded = @import("coded.zig");
 
 pub fn main() !void {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
+
+    var cfg = try coded.Config.init("CodedConfig");
+    defer cfg.deinit();
+
+    var itr = cfg.setBuf.iterator();
+    while (itr.next()) |elem| {
+        std.debug.print("{s}: {s}\n", .{ elem.key_ptr, elem.value_ptr });
+    }
 }
