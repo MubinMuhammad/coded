91c39590011171b8ccfd588feda3d38c6fe9649c
Mubin Muhammad <mubin.muhammad.the.7th@gmail.com>
2024-08-05 22:35:58 +0600

    working on reading config...10% done

--- src/config.zig
+++ src/config.zig
@@ -8,10 +8,12 @@ pub const ConfigError = error{
 
 pub const Config = struct {
     setBuf: std.BufMap,
-    themeBuf: []const u8,
-    pathBuf: std.BufMap,
+    theme: []const u8,
+    themeType: []const u8,
+    themeCallibration: bool,
+    paths: std.BufMap,
+    colorSchemes: std.BufMap,
 
-    // for later, this argument, internalConfigPath: []const u8
     pub fn init(configPath: []const u8) !Config {
         var cfg: Config = undefined;
         initConfig(&cfg);
@@ -25,25 +27,28 @@ pub const Config = struct {
         var lineBuf: [1024]u8 = undefined;
 
         while (try inputStream.readUntilDelimiterOrEof(&lineBuf, '\n')) |line| {
-            std.debug.print("{s}\n", .{line});
-            try getCommandArgs(line, &cfg);
+            var k: u32 = 0;
+            for (line) |c| {
+                if (c == '#') break;
+                k += 1;
+            }
+
+            try getCommandArgs(line[0..k], &cfg);
         }
 
         return cfg;
     }
 
     pub fn deinit(self: *Config) void {
-        self.*.pathBuf.deinit();
-        self.*.changeBuf.deinit();
-        self.*.themeBuf.deinit();
-        self.*.pathBuf.deinit();
+        self.*.setBuf.deinit();
+        self.*.colorSchemes.deinit();
+        self.*.paths.deinit();
     }
 
     fn initConfig(cfg: *Config) void {
         cfg.*.setBuf = std.BufMap.init(std.heap.page_allocator);
-        cfg.*.changeBuf = std.BufMap.init(std.heap.page_allocator);
-        cfg.*.themeBuf = std.BufMap.init(std.heap.page_allocator);
-        cfg.*.pathBuf = std.BufMap.init(std.heap.page_allocator);
+        cfg.*.colorSchemes = std.BufMap.init(std.heap.page_allocator);
+        cfg.*.paths = std.BufMap.init(std.heap.page_allocator);
     }
 
     fn getCommandArgs(line: []const u8, cfg: *Config) !void {
@@ -52,44 +57,33 @@ pub const Config = struct {
 
         var tkn = std.mem.split(u8, line, " ");
 
-        if (tkn.peek().?[0] == '#')
-            return;
-
         var command = std.ArrayList(u8).init(std.heap.page_allocator);
         defer command.deinit();
 
         if (tkn.peek().?.len == 1 and tkn.peek().?[0] == '/') {
-            try command.appendSlice(tkn.next().?);
+            _ = tkn.next();
             try command.appendSlice(tkn.next().?);
         } else {
-            try command.appendSlice(tkn.next().?);
+            try command.appendSlice(tkn.next().?[1..]);
         }
 
-        const storeBuf: *std.BufMap = try getBuf(command.items, cfg);
-        var keyVal: [2][128]u8 = undefined;
-        var k: u8 = 0;
-
-        while (tkn.next()) |elem| {
-            std.mem.copyForwards(u8, &keyVal[k], elem);
-            if (elem[0] == '#' or k >= 1) break;
-            k += 1;
-        }
-
-        if (k < 1) return error.TooFewArgs;
-        if (tkn.next() != null and tkn.peek().?[0] != '#') return error.TooManyArgs;
-
-        try storeBuf.*.put(&keyVal[0], &keyVal[1]);
-    }
-
-    fn getBuf(command: []const u8, cfg: *Config) !*anyopaque {
-        if (std.mem.eql(u8, command[1..], "set")) {
-            return &cfg.*.setBuf;
-        } else if (std.mem.eql(u8, command[1..], "changeTheme")) {
-            return &cfg.*.themeBuf;
-        } else if (std.mem.eql(u8, command[1..], "addPath")) {
-            return &cfg.*.pathBuf;
+        if (std.mem.eql(u8, command.items, "set")) {
+            try cfg.*.setBuf.put(tkn.next().?, tkn.next().?);
+        } else if (std.mem.eql(u8, command.items, "theme")) {
+            cfg.*.theme = tkn.next().?;
+        } else if (std.mem.eql(u8, command.items, "themeType")) {
+            cfg.*.themeType = tkn.next().?;
+        } else if (std.mem.eql(u8, command.items, "themeCallibration")) {
+            cfg.*.themeCallibration = if (std.mem.eql(u8, tkn.next().?, "true")) true else false;
+        } else if (std.mem.eql(u8, command.items, "addPath")) {
+            try cfg.*.paths.put(tkn.next().?, tkn.next().?);
+        } else if (std.mem.eql(u8, command.items, "addTheme")) {
+            try cfg.*.colorSchemes.put(tkn.next().?, tkn.next().?);
         } else {
             return error.InvalidCommand;
         }
+
+        // NOTE: Not working because of trailing spaces.
+        if (tkn.peek() != null) return error.TooManyArgs;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,14 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
 
-    var cfg = try coded.Config.init("CodedConfig");
+    var cfg = coded.Config.init("CodedConfig") catch |e| {
+        std.debug.print("Error loading config! [{?}]\n", .{e});
+        return;
+    };
     defer cfg.deinit();
 
     var itr = cfg.setBuf.iterator();
     while (itr.next()) |elem| {
-        std.debug.print("{s}: {s}\n", .{ elem.key_ptr, elem.value_ptr });
+        std.debug.print("{s}: {s}\n", .{ elem.key_ptr.*, elem.value_ptr.* });
     }
 }
